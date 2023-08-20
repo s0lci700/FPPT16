e8351d4a8b6c8a53b4e3371559956a6c0e7a8037
Sol León <solv.leong@gmail.com>
2023-08-20 17:59:14 -0400

    reset to original unified class view

--- Fichas/views.py
+++ Fichas/views.py
@@ -38,106 +38,106 @@ def ficha_detail_view(request, id):
     return render(request, "ficha_detail.html", {"ficha": ficha})
 
 
-class FichaCreateView(LoginRequiredMixin, CreateView):
-    model = Ficha
-    template_name = "ficha_create.html"
-    form_class = FichaForm
-
-    def form_valid(self, form):
-        ficha = form.save(commit=False)
-        ficha.status = "Borrador" if "save_draft" in self.request.POST else "Publicado"
-        student = self.request.user
-        student_profile = StudentProfile.objects.get(user=student)
-        ficha.student = student_profile
-        ficha.save()
-        return render(self.request, "ficha_partial.html", {"ficha": ficha})
-
-    def form_invalid(self, form):
-        response = super().form_invalid(form)
-        if self.request.is_ajax():
-            return JsonResponse(form.errors, status=400)
-        else:
-            return response
-
-
-class FichaUpdateView(LoginRequiredMixin, UpdateView):
-    model = Ficha
-    template_name = "ficha_update.html"
-    form_class = FichaForm
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        if self.request.is_ajax():
-            data = {"msg": "Update successful!"}
-            return JsonResponse(data)
-        else:
-            return response
-
-    def form_invalid(self, form):
-        response = super().form_invalid(form)
-        if self.request.is_ajax():
-            return JsonResponse(form.errors, status=400)
-        else:
-            return response
-
-
-class FichaDeleteView(LoginRequiredMixin, DeleteView):
-    model = Ficha
-    template_name = "ficha_delete.html"
-
-    def delete(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        self.object.delete()
-        data = {"msg": "Ficha deleted!"}
-        return JsonResponse(data)
-
-
-# @login_required
-# class FichaCreateUpdateDeleteView(
-#     LoginRequiredMixin, CreateView, UpdateView, DeleteView
-# ):
+# class FichaCreateView(LoginRequiredMixin, CreateView):
 #     model = Ficha
 #     template_name = "ficha_create.html"
 #     form_class = FichaForm
-#
-#     def get_object(self, queryset=None):
-#         if self.kwargs.get("pk"):
-#             return super().get_object(queryset)
-#         return None
-#
-#     def post(self, request, *args, **kwargs):
-#         if "delete" in request.POST:
-#             return render(
-#                 request, "ficha_delete_confirm.html", {"ficha": self.get_object()}
-#             )
-#         elif "confirm_delete" in request.POST:
-#             # Check password confirmation here
-#             if request.user.check_password(request.POST["password"]):
-#                 return self.delete(request, *args, **kwargs)
-#             else:
-#                 return render(
-#                     request,
-#                     "ficha_delete_confirm.html",
-#                     {"ficha": self.get_object(), "error": "Incorrect password"},
-#                 )
-#         return super().post(request, *args, **kwargs)
-#
+
 #     def form_valid(self, form):
 #         ficha = form.save(commit=False)
-#         if "save_draft" in self.request.POST:
-#             ficha.status = "Borrador"
-#         else:
-#             ficha.status = "Publicado"
+#         ficha.status = "Borrador" if "save_draft" in self.request.POST else "Publicado"
 #         student = self.request.user
 #         student_profile = StudentProfile.objects.get(user=student)
 #         ficha.student = student_profile
 #         ficha.save()
-#         return super().form_valid(form)
-#
-#     def get_success_url(self):
-#         return reverse_lazy(
-#             "ficha_general"
-#         )  # Redirect to the list of fichas after successful action
+#         return render(self.request, "ficha_partial.html", {"ficha": ficha})
+
+#     def form_invalid(self, form):
+#         response = super().form_invalid(form)
+#         if self.request.is_ajax():
+#             return JsonResponse(form.errors, status=400)
+#         else:
+#             return response
+
+
+# class FichaUpdateView(LoginRequiredMixin, UpdateView):
+#     model = Ficha
+#     template_name = "ficha_update.html"
+#     form_class = FichaForm
+
+#     def form_valid(self, form):
+#         response = super().form_valid(form)
+#         if self.request.is_ajax():
+#             data = {"msg": "Update successful!"}
+#             return JsonResponse(data)
+#         else:
+#             return response
+
+#     def form_invalid(self, form):
+#         response = super().form_invalid(form)
+#         if self.request.is_ajax():
+#             return JsonResponse(form.errors, status=400)
+#         else:
+#             return response
+
+
+# class FichaDeleteView(LoginRequiredMixin, DeleteView):
+#     model = Ficha
+#     template_name = "ficha_delete.html"
+
+#     def delete(self, request, *args, **kwargs):
+#         self.object = self.get_object()
+#         self.object.delete()
+#         data = {"msg": "Ficha deleted!"}
+#         return JsonResponse(data)
+
+
+@login_required
+class FichaCreateUpdateDeleteView(
+    LoginRequiredMixin, CreateView, UpdateView, DeleteView
+):
+    model = Ficha
+    template_name = "ficha_create.html"
+    form_class = FichaForm
+
+    def get_object(self, queryset=None):
+        if self.kwargs.get("pk"):
+            return super().get_object(queryset)
+        return None
+
+    def post(self, request, *args, **kwargs):
+        if "delete" in request.POST:
+            return render(
+                request, "ficha_delete_confirm.html", {"ficha": self.get_object()}
+            )
+        elif "confirm_delete" in request.POST:
+            # Check password confirmation here
+            if request.user.check_password(request.POST["password"]):
+                return self.delete(request, *args, **kwargs)
+            else:
+                return render(
+                    request,
+                    "ficha_delete_confirm.html",
+                    {"ficha": self.get_object(), "error": "Incorrect password"},
+                )
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        ficha = form.save(commit=False)
+        if "save_draft" in self.request.POST:
+            ficha.status = "Borrador"
+        else:
+            ficha.status = "Publicado"
+        student = self.request.user
+        student_profile = StudentProfile.objects.get(user=student)
+        ficha.student = student_profile
+        ficha.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "ficha_general"
+        )  # Redirect to the list of fichas after successful action
 
 
 # @login_required
